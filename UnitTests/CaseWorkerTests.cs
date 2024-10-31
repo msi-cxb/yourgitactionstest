c01070d3f6a58e4bc87e879a509f1564b96d072b
charlie buckheit <charlie.buckheit@mercurysolutions.com>
2024-10-31 12:20:59 -0300

    update unit test for michelle

--- UnitTests/CaseWorkerTests.cs
+++ UnitTests/CaseWorkerTests.cs
@@ -13,6 +13,7 @@ namespace UnitTests
             Assert.IsTrue((int)Cases.CaseWorkers.Joe == 1);
             Assert.IsTrue((int)Cases.CaseWorkers.Jane == 2);
             Assert.IsTrue((int)Cases.CaseWorkers.Mark == 3);
+            Assert.IsTrue((int)Cases.CaseWorkers.Michelle == 4);
         }
 
         [TestMethod]
