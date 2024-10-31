28c28d5cb8e0af18747299d7d2b524385337fa13
charlie buckheit <charlie.buckheit@mercurysolutions.com>
2024-10-31 15:16:52 -0300

    make sure unit test fails

--- UnitTests/CaseWorkerTests.cs
+++ UnitTests/CaseWorkerTests.cs
@@ -19,7 +19,7 @@ namespace UnitTests
         [TestMethod]
         public void TestCaseWorkersCount()
         {
-            Assert.IsTrue(Enum.GetNames(typeof(Cases.CaseWorkers)).Length == 4);
+            Assert.IsTrue(Enum.GetNames(typeof(Cases.CaseWorkers)).Length == 3);
         }
     }
 }
