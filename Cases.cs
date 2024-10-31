6bf482d046cb7e7ba07b315204f48fda5564ad66
charlie buckheit <charlie.buckheit@mercurysolutions.com>
2024-10-31 11:19:34 -0300

    adding bob

--- Cases.cs
+++ Cases.cs
@@ -13,7 +13,8 @@ namespace gitactionstest
             Joe = 1,
             Jane = 2,
             Michelle = 3,
-            Mark = 4
+            Bob = 4,
+            Mark = 5
         }
     }
 }
